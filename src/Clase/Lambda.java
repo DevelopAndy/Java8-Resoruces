231fae112c3a8f7fb17238f9a3bf913567d69a78
Andy <ander1386@gmail.com>
2022-09-08 08:09:48 -0500

    using lambdas

---
+++ src/Clase/Lambda.java
@@ -0,0 +1,27 @@
+package Clase;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class Lambda {
+	
+	public static void main(String[] args) {
+
+		List<String> cursos = new ArrayList<>();
+        cursos.add("curso de fisica");
+        cursos.add("curso de java 8");
+        cursos.add("curso de geometria del espacio");
+        cursos.add("curso de historia universal");
+
+        cursos.sort((o1, o2) ->
+            Integer.compare(o2.length(), o1.length())
+        );
+
+        cursos.forEach(s -> { if(s.length() > 15) System.out.println(s);});
+    }
+}
+
+
